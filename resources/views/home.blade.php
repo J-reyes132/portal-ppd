9c0c9e35c8996d8cbbde96569b87d1153623a9be
Jreyes <juanreyes1321@outlook.com>
2024-06-22 12:58:55 -0400

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+@extends('layout.navbar')
+@section('title', 'Inicio')
+
+@section('content')
+
+{{-- <div class="text-center">
+        <h1 class="text-4xl font-bold">Bienvenido al portal de Programacion Paralela Distributiva</h1>
+        <p class="text-lg">Este portal trata sobre la programación paralela y distribuida.</p>
+    </div> --}}
